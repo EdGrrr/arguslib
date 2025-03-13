80a9f26894dce49688b1739c13196176fe7908e2
Oliver Driver <o.driver22@imperial.ac.uk>
2025-03-13 09:58:47 +0000

    Include test of plotting historic aircraft/camera data imported using config

---
+++ examples/aircraft_camera_interface_test.py
@@ -0,0 +1,12 @@
+# %%
+import datetime
+from pathlib import Path
+from arguslib.aircraft.camera_aircraft_interface import CameraAircraftInterface
+
+int = CameraAircraftInterface.from_campaign("COBALT", "3-7")
+dt = datetime.datetime(2025, 3, 9)
+adsb_datadir = Path("/disk1/Data/ADS-B/COBALT/")
+int.fleet.load_output(str(adsb_datadir / (dt.strftime("%Y%m%d") + "_ADS-B")))
+int.show(dt.replace(hour=13, minute=5), tlen=10 * 60)
+
+# %%
